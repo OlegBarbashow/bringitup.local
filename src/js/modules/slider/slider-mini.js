777acdde80b717179b68938141389f801650c8fd
Oleg Barbashow <teosofio@gmail.com>
2022-08-30 18:39:14 +0300

    Solving multipage errors(page: modules.html)

--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -56,26 +56,28 @@ export default class MiniSlider extends Slider{
     }
 
     init() {
-        this.container.style.cssText = `
+        try {
+            this.container.style.cssText = `
             display: flex;
             flex-wrap: wrap;
             overflow: hidden;
             align-items: flex-start;
         `;
 
-        this.bindTriggers();
-        this.decorizeSlides();
+            this.bindTriggers();
+            this.decorizeSlides();
 
-        if (this.autoplay) {
-            this.slideMover();
-
-            this.container.addEventListener('mouseenter', () => {
-                clearInterval(this.paused);
-            });
-
-            this.container.addEventListener('mouseleave', () => {
+            if (this.autoplay) {
                 this.slideMover();
-            });
-        }
+
+                this.container.addEventListener('mouseenter', () => {
+                    clearInterval(this.paused);
+                });
+
+                this.container.addEventListener('mouseleave', () => {
+                    this.slideMover();
+                });
+            }
+        } catch (e) {}
     }
 }
